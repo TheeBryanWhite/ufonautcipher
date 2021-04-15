33d78e56d5a2fcfeba318bf99af688a6972724c5
Bryan White <bwhite@eastcoastcatalyst.com>
2021-04-15 19:31:59 -0400

    Added more SEO stuff

--- src/components/QueryCard.js
+++ src/components/QueryCard.js
@@ -1,6 +1,6 @@
 /** @jsxRuntime classic */
 /** @jsx jsx */
-import { Component } from 'react';
+import { Component } from 'preact/compat';
 import { css, jsx } from '@emotion/react';
 import { 
 	Button,
