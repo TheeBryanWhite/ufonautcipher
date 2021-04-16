448a5f3b6eff9f1735afc964241ba4e18edbc20b
Bryan White <bwhite@eastcoastcatalyst.com>
2021-04-15 22:00:29 -0400

    Added lazy component

--- src/components/QueryCard.js
+++ src/components/QueryCard.js
@@ -1,6 +1,6 @@
 /** @jsxRuntime classic */
 /** @jsx jsx */
-import { Component } from 'react';
+import { Component, lazy } from 'react';
 import { css, jsx } from '@emotion/react';
 import TextField from '@material-ui/core/TextField';
 import Grid from '@material-ui/core/Grid';
@@ -9,10 +9,11 @@ import Card from '@material-ui/core/Card';
 import Button from '@material-ui/core/Button';
 import debounce from 'lodash.debounce';
 import InfoBar from './InfoBar';
-import ResultsPane from './ResultsPane';
 import EyeOfProvidence from '../svg/EyeOfProvidence';
 import queryEncode from '../utilities/queryEncode';
 import liberTextBreakdown from '../utilities/liberTextBreakdown';
+
+const ResultsPane = lazy(() => import('./ResultsPane'));
 
 class QueryCard extends Component {
 
